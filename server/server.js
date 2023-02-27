17a6230f2aead82341c77f741b8c5928a305ef71
KyeongsooLee <leeksca26@gmail.com>
2023-02-26 23:04:15 -0500

    OpenAI Server - 2

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-const Configuration = new Configuration({
+const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -26,9 +26,22 @@ app.post('/', async (req, res) => {
         const prompt = req.body.prompt;
 
         const response = await openai.createCompletion({
-            
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
         })
     } catch (error){
-
+        console.log(error);
+        res.status(500).send({ error })
     }
 })
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
