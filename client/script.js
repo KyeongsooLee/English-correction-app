403bf65aa108d6f50a7ffd755514fd9bca27b389
KyeongsooLee <leeksca26@gmail.com>
2023-04-13 21:00:45 -0400

    added example function

--- client/script.js
+++ client/script.js
@@ -3,6 +3,7 @@ import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
+const otherButton = document.querySelector('#example_button');
 
 let loadInterval;
 
@@ -119,3 +120,53 @@ form.addEventListener('keyup', (e) => {
     handleSubmit(e);
   }
 })
+
+otherButton.addEventListener('click', async (e) => {
+  const prompt = "Could you generate an article about online learning for English learners? It should be 3 paragraph including intro, body, and conclusion, but please don't put the words of intro, body, conclusion.";
+  const prompt_screen = "Short Article about online learning"
+  e.preventDefault();
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, prompt_screen);
+
+  // to clear the textarea input 
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId);
+  
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv);
+
+  const response = await fetch('https://chatgpt-clone-hgmk.onrender.com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
+
+  if(response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n' 
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+});
